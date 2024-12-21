94664fbba5e31cdd117c1e1cf99fe81ab240eaba
Xin ZF <43563921+TremblingV5@users.noreply.github.com>
2024-12-22 00:28:08 +0800

    add check protovalidate env method (#94)
    ## Link to an issue
    
    <!-- Please add an issue link if this PR is related with one. -->
    
    ## What
    
    <!-- Please describe what this PR does in simple words. -->
    
    ## How
    
    <!-- Please describe how you have implemented the changes. -->
    
    ## Screenshots
    
    <!-- Please add screenshots if we need. -->
    
    ## How to test
    
    <!-- Please describe how to test the changes you have made. -->
    
    ## Checklist
    
    - [ ] Link to an issue if it really related.
    - [ ] At least describe what this PR does.
    - [ ] Use `rebase` to confirm that current branch doesn't conflict with
    main branch.
    - [ ] Unit tests. At least do not reduce the unit test coverage.
    - [ ] Checked and updated `guidelines.md` which used to describe how to
    build, deploy and use DouTok.
    
    Co-authored-by: TremblingV5 <zf.xin@qq.com>

--- backend/gopkgs/middlewares/protobufvalidator/middleware.go
+++ backend/gopkgs/middlewares/protobufvalidator/middleware.go
@@ -8,6 +8,28 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+func doValidate(req proto.Message) error {
+	v, err := protovalidate.New()
+	if err != nil {
+		return err
+	}
+
+	if err := v.Validate(req); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CheckEnv() error {
+	_, err := protovalidate.New()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func ProtobufValidator() middleware.Middleware {
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (interface{}, error) {
@@ -16,12 +38,7 @@ func ProtobufValidator() middleware.Middleware {
 				return nil, errors.New("not valid request")
 			}
 
-			v, err := protovalidate.New()
-			if err != nil {
-				return nil, err
-			}
-
-			if err := v.Validate(value); err != nil {
+			if err := doValidate(value); err != nil {
 				return nil, err
 			}
 
